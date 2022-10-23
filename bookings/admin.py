7b9a52093a4736ba6c0868eeb8a8a1be3b5ab8e1
OwenDa <owendarcy25@gmail.com>
2022-10-23 19:08:36 +0000

    Set up BookingAdmin in bookings/admin.py

--- bookings/admin.py
+++ bookings/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import Booking
 
 
-admin.site.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('booking_id',)
+
+    fields = ('first_name', 'last_name', 'email', 'created_at', 'booking_id')
+
+    list_display = ('booking_id', 'created_at', 'first_name',
+                    'last_name',)
+
+    ordering = ('created_at',)
+
+
+admin.site.register(Booking, BookingAdmin)
