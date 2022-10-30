0e541de99e8b7a80014f8f9d46d0c22da883fd63
OwenDa <owendarcy25@gmail.com>
2022-10-30 20:13:14 +0000

    Render card verification errors in checkout.html errorDiv

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -4,8 +4,9 @@ Employed within checkout.html and checkout procedures
 
 let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1,);
 let clientSecret = $('#id_client_secret').text().slice(1, -1);
-
 let stripe = Stripe(stripePublicKey);
+
+// Add Stripe Card Field to Checkout Form
 let elements = stripe.elements();
 
 let style = {
@@ -26,3 +27,21 @@ let style = {
 
 let card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle card validation errors:
+card.addEventListener('change', function (event) {
+    console.log('js stage one')
+    let errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = "";
+    }
+});
+
