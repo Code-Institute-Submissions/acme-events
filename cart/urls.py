1712ba5725ee3bebc56f2f4a8c73499dcc2c878d
OwenDa <owendarcy25@gmail.com>
2022-10-31 22:21:11 +0000

    Add ability to update qty. of specific item in cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cart/', views.cart, name='cart'),
+    path('view-cart/', views.view_cart, name='view_cart'),
     path('add/<event_id>', views.add_to_cart, name='add_to_cart'),
+    path('amend-cart/<event_id>/', views.amend_cart, name='amend_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib import messages
 
 from events.models import Event
 
 
-def cart(request):
+def view_cart(request):
     """ View shopping cart """
     page_specific_title = 'Your Cart'
     context = {
@@ -32,3 +32,22 @@ def add_to_cart(request, event_id):
     request.session['cart'] = cart
 
     return redirect(redirect_url)
+
+
+def amend_cart(request, event_id):
+    """Adjust the quantity of the specified item(s)"""
+
+    event = get_object_or_404(Event, pk=event_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[event_id] = quantity
+        messages.success(request, f'Quantity updated successfully.')
+    else:
+        cart.pop(event_id)
+        messages.success(request, f'Removed {event.name} from your bag')
+
+    request.session['cart'] = cart
+
+    return redirect(reverse('view_cart'))
