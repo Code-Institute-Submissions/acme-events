de5a5266e6503a9bdddf5b4b58946fe48e232bee
OwenDa <owendarcy25@gmail.com>
2022-11-18 13:58:52 +0000

    Add fields to BookingAdmin

--- bookings/admin.py
+++ bookings/admin.py
@@ -6,7 +6,10 @@ class BookingAdmin(admin.ModelAdmin):
 
     readonly_fields = ('booking_id', 'created_at', )
 
-    fields = ('first_name', 'last_name', 'email', 'created_at', 'booking_id')
+    fields = ('first_name', 'last_name', 'street_address1',
+              'street_address2', 'city_or_town', 'county', 'postcode',
+              'country', 'telephone', 'email', 'created_at',
+              'booking_id', 'booking_total',)
 
     list_display = ('booking_id', 'created_at', 'first_name',
                     'last_name',)
