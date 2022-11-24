00df2534c86cb53c1ae2fa18f6ff45cef38f0984
OwenDa <owendarcy25@gmail.com>
2022-11-24 11:22:15 +0000

    Test: Refactor 404 + 500 handling

--- acme_events/urls.py
+++ acme_events/urls.py
@@ -1,6 +1,7 @@
 """
 acme_events project-level URL Configuration
 """
+from .views import handler404, handler500
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -15,3 +16,6 @@ urlpatterns = [
     path('', include('cart.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'acme_events.views.handler404'
+handler500 = 'acme_events.views.handler500'
